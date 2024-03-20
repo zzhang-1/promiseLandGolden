db00a03fd299c760beaddcc051d8acd44276365d
Zhen Zhang <czhang36@umd.edu>
2024-03-20 19:06:08 -0400

    initialize backend project

---
+++ backEnd/goldenRetriever/src/main/java/com/promiseLand/goldenRetriever/GoldenRetrieverApplication.java
@@ -0,0 +1,13 @@
+package com.promiseLand.goldenRetriever;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoldenRetrieverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoldenRetrieverApplication.class, args);
+	}
+
+}
---
+++ backEnd/goldenRetriever/src/test/java/com/promiseLand/goldenRetriever/GoldenRetrieverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.promiseLand.goldenRetriever;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoldenRetrieverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
